9e5b59f3cf72ee2f30390dc376a2eaf340266e0a
Asif Bhuiyan <67231225+asifbhuiyann@users.noreply.github.com>
2022-08-26 19:21:20 +0600

    Update Main.java
    Some of my personal info was in the code and I've washed that out.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-/*
- Asif Bhuiyan (2011941642)
- Ratul Bhattarcharjee(2012996642)
- Mohammad Olid Afzal(2011831042)
- Group No: 05
- Section:5
- */
 import javax.swing.JFrame;
 
 public class Main {
